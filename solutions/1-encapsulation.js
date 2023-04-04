491c9638f2712833ce19c90a93ab216d7418f244
IvanSilin <siliivan944@gmail.com>
2023-04-04 12:08:01 +0300

    first commit

--- solutions/1-encapsulation.js
+++ solutions/1-encapsulation.js
@@ -1,5 +1,19 @@
 // BEGIN
+export const getMutualFriends = (userOne, userTwo) =>{
 
+  const mutualFriends = [];
+  const userTwoFriends = new Set(userTwo.getFriends().map(friend => friend.id));
+
+  for(let friend of userOne.getFriends()){
+
+    if (userTwoFriends.has(friend.id)){
+
+      mutualFriends.push(friend);
+    }
+  }
+
+  return mutualFriends;
+};
 // END
 
 export default ({ id = null, friends = [] } = {}) => ({
--- solutions/2-this.js
+++ solutions/2-this.js
@@ -1,3 +1,47 @@
 // BEGIN
-
+export const make = (numer = 0, denom = 1) =>{
+    let privateNumer = numer;
+    let privateDenom = denom;
+    
+    function gcd(a, b) {
+      if (b === 0) {
+        return a;
+      }
+      return gcd(b, a % b);
+    }
+    
+    function reduce() {
+      const g = gcd(privateNumer, privateDenom);
+      privateNumer /= g;
+      privateDenom /= g;
+    }
+  
+    return {
+      setNumer(n) {
+        privateNumer = n;
+        reduce();
+      },
+      setDenom(d) {
+        privateDenom = d;
+        reduce();
+      },
+      getNumer() {
+        return privateNumer;
+      },
+      getDenom() {
+        return privateDenom;
+      },
+      toString() {
+        return `${privateNumer}/${privateDenom}`;
+      },
+      add(other) {
+        const newNumer = privateNumer * other.getDenom() + privateDenom * other.getNumer();
+        const newDenom = privateDenom * other.getDenom();
+        return make(newNumer, newDenom);
+      },
+    };
+  }
+  
+  export default make;
+  
 // END
